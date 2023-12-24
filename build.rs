c7b03ea73e2c720c50b8aa903a82ff047e03b7f7
Fenhl <fenhl@fenhl.net>
2023-12-24 17:41:31 +0000

    Dont generate Windows resource for non-Windows targets

--- build.rs
+++ build.rs
@@ -1,13 +1,19 @@
 use {
-    std::io,
+    std::{
+        env,
+        io,
+    },
     winres::WindowsResource,
 };
 
 fn main() -> io::Result<()> {
-    WindowsResource::new()
-        //TODO icon?
-        //TODO version info?
-        .set_manifest_file("assets/manifest.xml")
-        //TODO embed systray icons instead of loading dynamically, see https://github.com/gabdube/native-windows-gui/tree/master/native-windows-gui/examples/embed_resources
-        .compile()
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        WindowsResource::new()
+            //TODO icon?
+            //TODO version info?
+            .set_manifest_file("assets/manifest.xml")
+            //TODO embed systray icons instead of loading dynamically, see https://github.com/gabdube/native-windows-gui/tree/master/native-windows-gui/examples/embed_resources
+            .compile()?;
+    }
+    Ok(())
 }
